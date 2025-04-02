85d7a0de388d10408fff87b9f0ad2d1420e58595
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-04-02 11:07:26 +0100

    fix

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,8 +1,27 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  HttpStatus,
+  Put,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { Artist } from './schemas/artist.schema';
+import { successResponse } from 'src/core/config/response';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { UpdateArtisteDto } from './dto/update-artist.dto';
 
 @ApiTags('artists')
 @Controller('api/v1/artists')
@@ -19,14 +38,26 @@ export class ArtistController {
   })
   @ApiResponse({ status: 400, description: 'Bad Request' })
   async create(@Body() createArtistDto: CreateArtistDto) {
-    return await this.artistService.create(createArtistDto);
+    const data = await this.artistService.create(createArtistDto);
+    return successResponse({
+      message: 'Artist created successfully',
+      code: HttpStatus.OK,
+      status: 'success',
+      data,
+    });
   }
 
   @Get()
   @ApiOperation({ summary: 'Get all artists' })
   @ApiResponse({ status: 200, description: 'List of artists', type: [Artist] })
   async findAll() {
-    return await this.artistService.findAll();
+    const data = await this.artistService.findAll();
+    return successResponse({
+      message: 'List of artists',
+      code: HttpStatus.OK,
+      status: 'success',
+      data,
+    });
   }
 
   @Get(':id')
@@ -34,6 +65,57 @@ export class ArtistController {
   @ApiResponse({ status: 200, description: 'Artist details', type: Artist })
   @ApiResponse({ status: 404, description: 'Artist not found' })
   async findOne(@Param('id') id: string) {
-    return await this.artistService.findOne(id);
+    const data = await this.artistService.findOne(id);
+    return successResponse({
+      message: 'Artist details',
+      code: HttpStatus.OK,
+      status: 'success',
+      data,
+    });
+  }
+
+  @Get('profile/me')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get logged-in artist profile' })
+  @ApiResponse({ status: 200, description: 'Artist profile', type: Artist })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 404, description: 'Artist not found' })
+  async getProfile(@Req() req: any) {
+    const data = await this.artistService.findOne(req.user['userId']);
+    return successResponse({
+      message: 'Artist profile',
+      code: HttpStatus.OK,
+      status: 'success',
+      data,
+    });
+  }
+
+  @Put('profile/me')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Update logged-in artist profile (excluding email)',
+  })
+  @ApiBody({ type: UpdateArtisteDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Artist profile updated',
+    type: Artist,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 404, description: 'Artist not found' })
+  async updateProfile(
+    @Req() req: any,
+    @Body() updateArtistDto: UpdateArtisteDto,
+  ) {
+    const userId = req.user.userId;
+    const data = await this.artistService.update(userId, updateArtistDto);
+    return successResponse({
+      message: 'Artist profile updated',
+      code: HttpStatus.OK,
+      status: 'success',
+      data,
+    });
   }
 }
